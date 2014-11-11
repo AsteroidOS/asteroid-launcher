797b89ee36bb0c4dd30c1d4fa6ba31724413c8ab
Aleksi Suomalainen <suomalainen.aleksi@gmail.com>
2014-11-11 17:24:41 +0200

    [version] 0.16

---
+++ src/qml/scripts/rotation.js
@@ -0,0 +1,55 @@
+/****************************************************************************************
+**
+** Copyright (C) 2014 Aleksi Suomalainen <suomalainen.aleksi@gmail.com>
+** All rights reserved.
+**
+** You may use this file under the terms of BSD license as follows:
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**     * Redistributions in binary form must reproduce the above copyright
+**       notice, this list of conditions and the following disclaimer in the
+**       documentation and/or other materials provided with the distribution.
+**     * Neither the name of the author nor the
+**       names of its contributors may be used to endorse or promote products
+**       derived from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+** ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+****************************************************************************************/
+
+function rotateObject(obj, r, nr) {
+
+    if (obj.rotation !== r) {
+        var nativeIsPortrait = ((nr === 0) || (nr === 180))
+        var isPortrait = ((r === 0) || (r === 180))
+
+        var correction = 0
+        var isNative=((nativeIsPortrait || isPortrait) && !(nativeIsPortrait && isPortrait)) //xor
+        //xor
+        if ((isNative || !nativeIsPortrait) && !(isNative && !nativeIsPortrait)) {
+          correction = obj.width / 2 - obj.height / 2
+        }
+        var diff = Math.abs(r - obj.rotation)
+        obj.rotation = r
+        if ((diff === 90) || (diff === 270)) {
+            console.log("rotateObject swapping w,h")
+            var w = obj.width
+            obj.width = obj.height
+            obj.height = w
+        }
+        obj.x = correction
+        obj.y = -correction
+    }
+}
