7a742a89d4e7e7587d6d3369e5da807b0495a725
Filip Matijević <filip.matijevic.pz@gmail.com>
2014-11-14 12:05:04 +0100

    [rotation] push nativeOrientation variable into qml context

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
             nativeOrientation = app.primaryScreen()->nativeOrientation();
         }
     }
-    app.engine()->rootContext()->setContextProperty("nativeOrientation", v);
+    app.engine()->rootContext()->setContextProperty("nativeOrientation", nativeOrientation);
     qmlRegisterType<GlacierWindowModel>("org.nemomobile.glacier", 1, 0 ,"GlacierWindowModel");
     app.setQmlPath("/usr/share/lipstick-glacier-home-qt5/qml/MainScreen.qml");
     // Give these to the environment inside the lipstick homescreen
