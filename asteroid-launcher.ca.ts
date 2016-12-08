75f7280aa226766edfb088db0d51b2f1913ba864
Marc Celeiro Escribà <maceleiro@gmail.com>
2016-12-08 16:53:54 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.ca.ts
+++ asteroid-launcher.ca.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>No s'han obert aplicacions</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Cap notificació</translation>
     </message>
 </context>
 </TS>
