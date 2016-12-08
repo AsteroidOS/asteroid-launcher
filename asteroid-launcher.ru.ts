851eb2e12a93c7ee09f89e07b125023071a19829
Anton Chernov <antonvchernov95@gmail.com>
2016-12-08 18:08:43 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.ru.ts
+++ asteroid-launcher.ru.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет открытых приложений</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Уведомлений нет</translation>
     </message>
 </context>
 </TS>
