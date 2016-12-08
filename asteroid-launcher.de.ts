2334d8877ef2e6c811873ab1125e582182b00688
Rico Brase <kontakt@rico-brase.de>
2016-12-08 11:43:41 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.de.ts
+++ asteroid-launcher.de.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Apps geöffnet</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Benachrichtigungen</translation>
     </message>
 </context>
 </TS>
