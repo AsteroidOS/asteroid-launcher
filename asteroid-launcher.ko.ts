835fea7bec41b35cc85c7c14d15728f45136d84c
snowmaan <gqbigpaps@gmail.com>
2016-12-09 08:55:33 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.ko.ts
+++ asteroid-launcher.ko.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>실행중인 앱 없음</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>알림 없음</translation>
     </message>
 </context>
 </TS>
