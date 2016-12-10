042e9e9286acbca1a500b10390bcac3d8e61315d
Dominik Danelski <dominik+foss@danelski.pl>
2016-12-10 13:56:53 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (3 of 3 strings)

--- i18n/asteroid-launcher.pl.ts
+++ i18n/asteroid-launcher.pl.ts
@@ -10,7 +10,7 @@
     <message>
         <location filename="../qml/AppSwitcher.qml" line="120"/>
         <source>No open apps</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak otwartych aplikacji</translation>
     </message>
 </context>
 <context>
