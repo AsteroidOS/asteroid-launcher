4d3efba5e99394856a9d1f33f64593df82d0a921
squale76 <grippons@gmail.com>
2016-12-13 06:59:26 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.fr.ts
+++ asteroid-launcher.fr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas d'applis ouvertes</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de notifications</translation>
     </message>
 </context>
 </TS>
