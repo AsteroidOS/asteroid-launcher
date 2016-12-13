763734fd50c268a2aa59030de90e5400af40ff24
dr_gogeta86 <dr_gogeta86@messinalug.org>
2016-12-13 08:50:39 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.it.ts
+++ asteroid-launcher.it.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Nessuna app aperta</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Nessuna notifica</translation>
     </message>
 </context>
 </TS>
