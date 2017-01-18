cab10438c43229423d99bfa28ebbb4b371795ae0
Vadim <titanv3585@gmail.com>
2017-01-18 17:01:39 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.uk.ts
+++ asteroid-launcher.uk.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Немає відкритих программ</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Немає сповіщень</translation>
     </message>
 </context>
 </TS>
