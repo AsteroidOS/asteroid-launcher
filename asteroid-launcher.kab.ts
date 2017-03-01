233a5ae11be4e5330e5c6d17d1373747dc158bcf
Muḥend Belqasem <belkacem77@gmail.com>
2017-03-01 14:34:31 +0000

    Added translation using Weblate (Kabyle)

---
+++ asteroid-launcher.kab.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kab">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="qml/AppSwitcher.qml" line="118"/>
+        <source>No apps opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeedsPage</name>
+    <message>
+        <location filename="qml/FeedsPage.qml" line="117"/>
+        <source>No notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
