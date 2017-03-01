4ec48c6a224405b86dcdefa5bac7e522c077b2bc
Muḥend Belqasem <belkacem77@gmail.com>
2017-03-01 14:35:07 +0000

    Translated using Weblate (Kabyle)
    Currently translated at 100.0% (2 of 2 strings)

--- asteroid-launcher.kab.ts
+++ asteroid-launcher.kab.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="qml/AppSwitcher.qml" line="118"/>
         <source>No apps opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Ulac isnasen yeldin</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="qml/FeedsPage.qml" line="117"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Ulac ilɣuten</translation>
     </message>
 </context>
 </TS>
