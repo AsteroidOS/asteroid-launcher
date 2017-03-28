ac5fb457c02c3cab286090f62b602eb0ca23fee3
Severi Hofslagare <zevez90@gmail.com>
2017-03-28 20:28:07 +0000

    Added translation using Weblate (Finnish)

---
+++ i18n/asteroid-launcher.fi.ts
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <source>No apps opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSwitcher.qml" line="120"/>
+        <source>No open apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeedsPage</name>
+    <message>
+        <location filename="../qml/FeedsPage.qml" line="119"/>
+        <source>No notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
