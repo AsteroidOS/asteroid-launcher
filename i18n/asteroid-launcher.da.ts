d73cafcbdfcd615955802093dfbb511c3c85bc50
Laust Lund Kristensen <laustijapan@hotmail.com>
2017-06-21 08:42:56 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-launcher.da.ts
+++ i18n/asteroid-launcher.da.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../qml/AppSwitcher.qml" line="118"/>
         <source>No open apps</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen åbne apps</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/FeedsPage.qml" line="118"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen beskeder</translation>
     </message>
 </context>
 </TS>
