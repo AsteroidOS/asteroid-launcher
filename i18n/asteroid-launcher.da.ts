2684d658ad2eed89fba382ff11a33771d7d914e1
Laust Lund Kristensen <laustijapan@hotmail.com>
2017-06-21 08:42:29 +0000

    Added translation using Weblate (Danish)

---
+++ i18n/asteroid-launcher.da.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../qml/AppSwitcher.qml" line="118"/>
+        <source>No open apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeedsPage</name>
+    <message>
+        <location filename="../qml/FeedsPage.qml" line="118"/>
+        <source>No notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
