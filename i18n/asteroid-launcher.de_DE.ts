20a398b44d83a0ccde5659639ed50529d46f837f
anonymous <>
2017-09-28 15:30:47 +0000

    Translated using Weblate (German)
    Currently translated at 70.5% (12 of 17 strings)

--- i18n/asteroid-launcher.de_DE.ts
+++ i18n/asteroid-launcher.de_DE.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="180"/>
         <source>Pass Key:</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort:</translation>
     </message>
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="195"/>
