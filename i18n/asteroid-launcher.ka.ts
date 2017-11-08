a8487b15809c0667065d0db6fac513169b421ccb
sam bowlby <sam@chven.us>
2017-11-08 17:18:38 +0000

    Added translation using Weblate (Georgian)

---
+++ i18n/asteroid-launcher.ka.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>BluetoothAgent</name>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="147"/>
+        <source>Authorize:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="158"/>
+        <source>Authorize</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="169"/>
+        <source>Confirm:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="180"/>
+        <source>Pass Key:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="195"/>
+        <source>Enter Key</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="202"/>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="213"/>
+        <source>PIN Code:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/misc/BluetoothAgent.qml" line="217"/>
+        <source>Enter PIN Code</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationActions</name>
+    <message>
+        <location filename="../qml/notifications/NotificationActions.qml" line="50"/>
+        <source>Snooze</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationActions.qml" line="56"/>
+        <source>Dismiss all</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationActions.qml" line="70"/>
+        <source>Dismiss</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationView</name>
+    <message>
+        <location filename="../qml/notifications/NotificationView.qml" line="55"/>
+        <source>Now</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationView.qml" line="59"/>
+        <source>m</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationView.qml" line="63"/>
+        <source>h</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationView.qml" line="66"/>
+        <source>d</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/notifications/NotificationView.qml" line="153"/>
+        <source>Dismiss</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationsPanel</name>
+    <message>
+        <location filename="../qml/notifications/NotificationsPanel.qml" line="148"/>
+        <source>No notifications</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Today</name>
+    <message>
+        <location filename="../qml/today/Today.qml" line="169"/>
+        <source>No events today</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
