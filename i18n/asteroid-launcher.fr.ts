77eb1fa6fc79c1d32181f7654f84357c63b542c8
Lionel SANCHEZ <wendigogo.fr@gmail.com>
2017-11-08 22:18:56 +0000

    Translated using Weblate (French)
    Currently translated at 5.8% (1 of 17 strings)

--- i18n/asteroid-launcher.fr.ts
+++ i18n/asteroid-launcher.fr.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="147"/>
         <source>Authorize:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Authentification :</translation>
     </message>
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="158"/>
         <source>Authorize</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Authentification</translation>
     </message>
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="169"/>
         <source>Confirm:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Confirmer :</translation>
     </message>
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="180"/>
         <source>Pass Key:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Mot de Passe :</translation>
     </message>
     <message>
         <location filename="../qml/misc/BluetoothAgent.qml" line="195"/>
