c758b74f54d4d45e580c44f79675dc5f21989335
Yaron Shahrabani <sh.yaron@gmail.com>
2017-11-25 13:20:37 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (15 of 15 strings)

--- i18n/asteroid-launcher.he.ts
+++ i18n/asteroid-launcher.he.ts
@@ -7,79 +7,79 @@
         <location filename="../qml/misc/BluetoothAgent.qml" line="143"/>
         <location filename="../qml/misc/BluetoothAgent.qml" line="155"/>
         <source>Authorize:</source>
-        <translation></translation>
+        <translation>אישור:</translation>
     </message>
     <message id="id-btagent-confirm">
         <location filename="../qml/misc/BluetoothAgent.qml" line="167"/>
         <source>Confirm:</source>
-        <translation></translation>
+        <translation>אימות:</translation>
     </message>
     <message id="id-btagent-passkey">
         <location filename="../qml/misc/BluetoothAgent.qml" line="179"/>
         <source>Pass Key:</source>
-        <translation></translation>
+        <translation>מפתח צופן:</translation>
     </message>
     <message id="id-btagent-enterkey">
         <location filename="../qml/misc/BluetoothAgent.qml" line="195"/>
         <source>Enter Key</source>
-        <translation></translation>
+        <translation>נא להזין מפתח</translation>
     </message>
     <message id="id-btagent-pincode">
         <location filename="../qml/misc/BluetoothAgent.qml" line="203"/>
         <location filename="../qml/misc/BluetoothAgent.qml" line="215"/>
         <source>PIN Code:</source>
-        <translation></translation>
+        <translation>קוד PIN:</translation>
     </message>
     <message id="id-btagent-enter-pincode">
         <location filename="../qml/misc/BluetoothAgent.qml" line="220"/>
         <source>Enter PIN Code</source>
-        <translation></translation>
+        <translation>נא להזין קוד PIN</translation>
     </message>
     <message id="id-snooze">
         <location filename="../qml/notifications/NotificationActions.qml" line="54"/>
         <source>Snooze</source>
-        <translation></translation>
+        <translation>נודניק</translation>
     </message>
     <message id="id-dismiss-all">
         <location filename="../qml/notifications/NotificationActions.qml" line="67"/>
         <source>Dismiss all</source>
-        <translation></translation>
+        <translation>התעלמות מהכול</translation>
     </message>
     <message id="id-dismiss">
         <location filename="../qml/notifications/NotificationActions.qml" line="82"/>
         <location filename="../qml/notifications/NotificationView.qml" line="152"/>
         <source>Dismiss</source>
-        <translation></translation>
+        <translation>התעלמות</translation>
     </message>
     <message id="id-now">
         <location filename="../qml/notifications/NotificationView.qml" line="56"/>
         <source>Now</source>
-        <translation></translation>
+        <translation>עכשיו</translation>
     </message>
     <message id="id-minute-abbrev">
         <location filename="../qml/notifications/NotificationView.qml" line="61"/>
         <source>m</source>
-        <translation></translation>
+        <translation>דק׳</translation>
     </message>
     <message id="id-hour-abbrev">
         <location filename="../qml/notifications/NotificationView.qml" line="66"/>
         <source>h</source>
-        <translation></translation>
+        <translation>שע׳</translation>
     </message>
     <message id="id-day-abbrev">
         <location filename="../qml/notifications/NotificationView.qml" line="70"/>
         <source>d</source>
-        <translation></translation>
+        <translation>ימ׳</translation>
     </message>
     <message id="id-no-notifications">
         <location filename="../qml/notifications/NotificationsPanel.qml" line="149"/>
         <source>No notifications</source>
-        <translation></translation>
+        <translation>אין התרעות</translation>
     </message>
     <message id="id-no-events-today">
         <location filename="../qml/today/Today.qml" line="185"/>
         <source>No events today</source>
-        <translation></translation>
+        <translation>אין אירועים היום</translation>
     </message>
 </context>
 </TS>
