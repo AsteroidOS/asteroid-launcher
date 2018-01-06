2e1503d86686f017c9eff8850ed23a4c28f25d90
Sebastian Rasmussen <sebras@gmail.com>
2018-01-06 16:16:52 +0000

    Translated using Weblate (Swedish)
    Currently translated at 68.4% (26 of 38 strings)

--- i18n/asteroid-launcher.sv.ts
+++ i18n/asteroid-launcher.sv.ts
@@ -148,7 +148,7 @@
     <message id="id-tutorial-notifications-subtitle">
         <location filename="../qml/firstrun/Tutorial.qml" line="95"/>
         <source>can be accessed on the left.</source>
-        <translation>kan bli åtkommen på vänster sida.</translation>
+        <translation>kan kommas åt på vänster sida.</translation>
     </message>
     <message id="id-tutorial-today-title">
         <location filename="../qml/firstrun/Tutorial.qml" line="122"/>
