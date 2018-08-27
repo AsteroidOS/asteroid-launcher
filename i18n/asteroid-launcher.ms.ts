480a93b7917bf28fd9f1786a749205702a5f9572
mohamad farid <mfarid.ahamid@gmail.com>
2018-08-27 06:07:32 +0000

    Translated using Weblate (Malay)
    Currently translated at 100.0% (38 of 38 strings)

--- i18n/asteroid-launcher.ms.ts
+++ i18n/asteroid-launcher.ms.ts
@@ -88,7 +88,7 @@
     <message id="id-today">
         <location filename="../qml/today/Today.qml" line="135"/>
         <source>Today</source>
-        <translation>Hari Ini</translation>
+        <translation>Hari ini</translation>
     </message>
     <message id="id-no-events-today">
         <location filename="../qml/today/Today.qml" line="206"/>
