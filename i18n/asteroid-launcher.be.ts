79e571cd8428213350e0f0b7a7840bd3fc3b7a4f
Toha <turok@duck.com>
2024-07-30 08:07:56 +0000

    Translated using Weblate (Belarusian)
    Currently translated at 100.0% (38 of 38 strings)

--- i18n/asteroid-launcher.be.ts
+++ i18n/asteroid-launcher.be.ts
@@ -7,56 +7,56 @@
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="158"/>
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="170"/>
         <source>Authorize:</source>
-        <translation type="unfinished"></translation>
+        <translation>Аўтарызаваць:</translation>
     </message>
     <message id="id-btagent-confirm">
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="182"/>
         <source>Confirm:</source>
-        <translation type="unfinished"></translation>
+        <translation>Пацвердзіць:</translation>
     </message>
     <message id="id-btagent-passkey">
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="194"/>
         <source>Pass Key:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ключ доступу:</translation>
     </message>
     <message id="id-btagent-enterkey">
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="210"/>
         <source>Enter Key</source>
-        <translation type="unfinished"></translation>
+        <translation>Увядзіце ключ</translation>
     </message>
     <message id="id-btagent-pincode">
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="218"/>
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="230"/>
         <source>PIN Code:</source>
-        <translation type="unfinished"></translation>
+        <translation>PIN-код:</translation>
     </message>
     <message id="id-btagent-enter-pincode">
         <location filename="../src/qml/misc/BluetoothAgent.qml" line="235"/>
         <source>Enter PIN Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Увядзіце PIN-код</translation>
     </message>
     <message id="id-snooze">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="115"/>
         <location filename="../src/qml/notifications/NotificationActions.qml" line="157"/>
         <source>Snooze</source>
-        <translation type="unfinished"></translation>
+        <translation>Адкласці</translation>
     </message>
     <message id="id-dismiss-all">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="123"/>
         <source>Dismiss all</source>
-        <translation type="unfinished"></translation>
+        <translation>Звольніць усіх</translation>
     </message>
     <message id="id-dismiss">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="138"/>
         <location filename="../src/qml/notifications/NotificationView.qml" line="161"/>
         <source>Dismiss</source>
-        <translation type="unfinished"></translation>
+        <translation>Звольніць</translation>
     </message>
     <message id="id-now">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="58"/>
         <location filename="../src/qml/notifications/NotificationView.qml" line="63"/>
         <source>Now</source>
-        <translation type="unfinished"></translation>
+        <translation>Зараз</translation>
     </message>
     <message id="id-minute-abbrev">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="63"/>
@@ -64,7 +64,7 @@
         <location filename="../src/qml/notifications/NotificationActions.qml" line="179"/>
         <location filename="../src/qml/notifications/NotificationView.qml" line="68"/>
         <source>m</source>
-        <translation type="unfinished"></translation>
+        <translation>м</translation>
     </message>
     <message id="id-hour-abbrev">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="68"/>
@@ -72,139 +72,139 @@
         <location filename="../src/qml/notifications/NotificationActions.qml" line="201"/>
         <location filename="../src/qml/notifications/NotificationView.qml" line="73"/>
         <source>h</source>
-        <translation type="unfinished"></translation>
+        <translation>г</translation>
     </message>
     <message id="id-day-abbrev">
         <location filename="../src/qml/notifications/NotificationActions.qml" line="72"/>
         <location filename="../src/qml/notifications/NotificationView.qml" line="77"/>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation>дз</translation>
     </message>
     <message id="id-no-notifications">
         <location filename="../src/qml/notifications/NotificationsPanel.qml" line="156"/>
         <source>No notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Няма апавяшчэнняў</translation>
     </message>
     <message id="id-today">
         <location filename="../src/qml/today/Today.qml" line="136"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Сення</translation>
     </message>
     <message id="id-no-events-today">
         <location filename="../src/qml/today/Today.qml" line="207"/>
         <source>No events today</source>
-        <translation type="unfinished"></translation>
+        <translation>Няма падзей сення</translation>
     </message>
     <message id="id-date-page">
         <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="201"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Дата</translation>
     </message>
     <message id="id-add-alarm">
         <location filename="../src/qml/firstrun/FakeAlarmclock.qml" line="85"/>
         <source>Add an alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>Дадаць будзільнік</translation>
     </message>
     <message id="id-time">
         <location filename="../src/qml/firstrun/FakeAlarmclock.qml" line="125"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Час</translation>
     </message>
     <message id="id-language-page">
         <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="171"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Мова</translation>
     </message>
     <message id="id-time-page">
         <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="190"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Час</translation>
     </message>
     <message id="id-tutorial-welcome-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="60"/>
         <source>Welcome.</source>
-        <translation type="unfinished"></translation>
+        <translation>Прываітанне.</translation>
     </message>
     <message id="id-tutorial-welcome-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="62"/>
         <source>This is AsteroidOS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Гэта AsteroidOS.</translation>
     </message>
     <message id="id-tutorial-indicators-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="73"/>
         <source>Indicators</source>
-        <translation type="unfinished"></translation>
+        <translation>Паказчыкі</translation>
     </message>
     <message id="id-tutorial-indicators-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="75"/>
         <source>show you possible gestures.</source>
-        <translation type="unfinished"></translation>
+        <translation>паказаць вам магчымыя жэсты.</translation>
     </message>
     <message id="id-tutorial-notifications-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="93"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Апавяшчэнні</translation>
     </message>
     <message id="id-tutorial-notifications-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="95"/>
         <source>can be accessed on the left.</source>
-        <translation type="unfinished"></translation>
+        <translation>можна атрымаць доступ злева.</translation>
     </message>
     <message id="id-tutorial-today-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="122"/>
         <source>Today&apos;s Events</source>
-        <translation type="unfinished"></translation>
+        <translation>Сённяшнія падзеі</translation>
     </message>
     <message id="id-tutorial-today-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="124"/>
         <source>are on the right.</source>
-        <translation type="unfinished"></translation>
+        <translation>знаходзяцца справа.</translation>
     </message>
     <message id="id-tutorial-quicksettings-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="171"/>
         <source>Quick Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Хуткія налады</translation>
     </message>
     <message id="id-tutorial-quicksettings-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="173"/>
         <source>are up here.</source>
-        <translation type="unfinished"></translation>
+        <translation>знаходзяцца тут.</translation>
     </message>
     <message id="id-tutorial-applications-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="220"/>
         <source>Apps</source>
-        <translation type="unfinished"></translation>
+        <translation>Праграмы</translation>
     </message>
     <message id="id-tutorial-applications-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="222"/>
         <source>are down there.</source>
-        <translation type="unfinished"></translation>
+        <translation>там унізе.</translation>
     </message>
     <message id="id-tutorial-startapp-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="269"/>
         <source>Start an app</source>
-        <translation type="unfinished"></translation>
+        <translation>Запусціце праграму</translation>
     </message>
     <message id="id-tutorial-startapp-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="271"/>
         <source>by pressing its icon.</source>
-        <translation type="unfinished"></translation>
+        <translation>націснуўшы яго значок.</translation>
     </message>
     <message id="id-tutorial-leaveapp-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="334"/>
         <source>Leave an app</source>
-        <translation type="unfinished"></translation>
+        <translation>Пакіньце праграму</translation>
     </message>
     <message id="id-tutorial-leaveapp-subtitle">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="336"/>
         <source>following a rightward gesture.</source>
         <oldsource>following a rightward gesture</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>пасля жэсту ўправа.</translation>
     </message>
     <message id="id-tutorial-enjoy-title">
         <location filename="../src/qml/firstrun/Tutorial.qml" line="400"/>
         <source>Enjoy!</source>
-        <translation type="unfinished"></translation>
+        <translation>Атрымлівайце асалоду!</translation>
     </message>
 </context>
 </TS>
