b0b8a923b1d4263fb445a89c3f760ec4cff4e522
GiannosOB <giannos2105@gmail.com>
2025-04-24 12:21:54 +0200

    Translated using Weblate (Greek)
    Currently translated at 100.0% (38 of 38 strings)

--- i18n/asteroid-launcher.el.ts
+++ i18n/asteroid-launcher.el.ts
@@ -83,7 +83,7 @@
     <message id="id-no-notifications">
         <location filename="../src/qml/notifications/NotificationsPanel.qml" line="156"/>
         <source>No notifications</source>
-        <translation>Καμία ειδοποίηση</translation>
+        <translation>Δεν υπάρχουν ειδοποιήσεις</translation>
     </message>
     <message id="id-today">
         <location filename="../src/qml/today/Today.qml" line="136"/>
