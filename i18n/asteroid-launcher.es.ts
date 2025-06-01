855c1ed1cf479012cc0e4c02ab0f4c271b0a444f
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2025-06-01 16:24:00 +0200

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (38 of 38 strings)

--- i18n/asteroid-launcher.es.ts
+++ i18n/asteroid-launcher.es.ts
@@ -83,7 +83,7 @@
     <message id="id-no-notifications">
         <location filename="../src/qml/notifications/NotificationsPanel.qml" line="156"/>
         <source>No notifications</source>
-        <translation>Sin notificaciones</translation>
+        <translation>No hay notificaciones</translation>
     </message>
     <message id="id-today">
         <location filename="../src/qml/today/Today.qml" line="136"/>
