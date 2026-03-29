5930bf4c5854b4c5e105e95de59b5277a7071398
moWerk <mo@mowerk.net>
2026-03-29 19:43:08 +0200

    FirstRunConfig: update translation strings after Time Zone rename
    Ran lupdate-qt5 to sync the id-timezone-page source string across all
    .ts translation files following the Timezone -> Time Zone correction. Also corrected the string in the english .ts file

--- i18n/asteroid-launcher.nl_BE.ts
+++ i18n/asteroid-launcher.nl_BE.ts
@@ -96,7 +96,7 @@
         <translation>Geen gebeurtenissen vandaag</translation>
     </message>
     <message id="id-date-page">
-        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="292"/>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="320"/>
         <source>Date</source>
         <translation>Datum</translation>
     </message>
@@ -111,19 +111,21 @@
         <translation>Tijd</translation>
     </message>
     <message id="id-language-page">
-        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="262"/>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="255"/>
         <source>Language</source>
         <translation>Taal</translation>
     </message>
     <message id="id-time-page">
-        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="281"/>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="289"/>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="309"/>
         <source>Time</source>
         <translation>Tijd</translation>
     </message>
     <message id="id-timezone-page">
-        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="304"/>
-        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="312"/>
-        <source>Timezone</source>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="274"/>
+        <location filename="../src/qml/firstrun/FirstRunConfig.qml" line="295"/>
+        <source>Time Zone</source>
+        <oldsource>Timezone</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message id="id-tutorial-welcome-title">
@@ -214,27 +216,29 @@
         <translation>Veel plezier!</translation>
     </message>
     <message id="id-brightness">
-        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="499"/>
-        <source>Brightness</source>
+        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="461"/>
+        <source></source>
+        <oldsource>Brightness</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message id="id-volume">
-        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="501"/>
-        <source>Volume</source>
+        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="468"/>
+        <source></source>
+        <oldsource>Volume</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message id="id-power-off">
-        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="839"/>
+        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="814"/>
         <source>Powering off in</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="id-tap-to-cancel">
-        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="543"/>
+        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="541"/>
         <source>Tap to cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="id-reboot">
-        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="856"/>
+        <location filename="../src/qml/quickpanel/QuickPanel.qml" line="829"/>
         <source>Rebooting in</source>
         <translation type="unfinished"></translation>
     </message>
